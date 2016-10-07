f9140e80ea68cf8bec05637d1ab2b56a4f715985
midnightapplepie <midnightapplepie@gmail.com>
2016-10-07 16:27:11 -0700

    added buttons

--- helpers/dare.js
+++ helpers/dare.js
@@ -1,11 +1,52 @@
 var request = require('request');
 
+function dareMessage(incomming){
+	var message = {
+		text: incomming.user_name + " dared you for a match?",
+		attachments: [
+			{
+				text: "kick his/her ass?",
+				fallback: "You been Challenged",
+				callback_id: "dared",
+				attachment_type: "default",
+				actions: [
+					{
+						name: "yes",
+						text: "Hell Yeah!",
+						type: "button",
+						value: "yes"
+					},
+					{
+						name: "no",
+						text: "Hell No",
+						type: "button",
+						value: "no"
+					},
+					{
+						name: "busy",
+						text: "Me = Busy",
+						type: "button",
+						value: "busy"
+					}
+				]
+			}
+		]
+	}
+}
+
 function handleMessage(incomming){
 	var users = incomming.text.match(/@\S+/g);
-	var response = {
-		"response_type" : "ephemeral",
-		"text" : "you dared " + users[0] + " for a match!!"
-	}
+	// var response = {
+	// 	"response_type" : "ephemeral"
+	// }
+	// if (users){	
+	// 	response.text = "you dared " + users[0]  + " for a match!!"
+	// }else{
+	// 	response.text = "who is " + incomming.text
+	// }
+
+	var response = dareMessage(incomming);
+
 	var outgoing = {
 		channel: "#" + incomming.channel_name,
 		channel_id: incomming.channel_id
@@ -21,7 +62,8 @@ function handleMessage(incomming){
 }
 
 var actions = {
-	handleMessage: handleMessage
+	handleMessage: handleMessage,
+	dareMessage: dareMessage
 }
 
 module.exports = actions;
